2dd71055b31850ff69dae5d15e717d055788481b
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2026-01-17 23:07:42 +0530

    Update program10.c

--- program10.c
+++ program10.c
@@ -3,7 +3,7 @@
     Step1 : Understand the problem statement
     Step2 : Write the algorithm
     Step3 : Decide the programming language
-    Step4 : Write the program
+    Step4 : Write the program 
     Step5 : Test the program
 */
 
